89eaf4143bb8c7bbfdc01770d6627bb4965e1d80
Lucas Queiroz <lucasestevam365@gmail.com>
2023-11-22 15:35:58 -0300

    Adicionando Singleton e testando

---
+++ PadroesDeProjeto/ConfiguracaoSingleton.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PadroesDeProjeto
+{
+    internal class ConfiguracaoSingleton
+    {
+        // garantir que esta configuração seja unica, não abrindo ambiguiade
+        private static ConfiguracaoSingleton instacia;
+
+        private string _url { get; set; }
+        private int _versao { get; set; }
+        private int _porta { get; set; }
+
+        public string Url { get { return _url; } }
+        public int Versao { get { return _versao; } }
+        public int Porta  { get { return _porta; } }
+
+        private ConfiguracaoSingleton()
+        {
+            _url = "localhost";
+            _versao = 1;
+            _porta = 8080;
+        }
+
+        public static ConfiguracaoSingleton getInstance()
+        {
+            if (instacia == null)
+                instacia = new ConfiguracaoSingleton();
+
+            return instacia;
+        }
+
+        public void ExibirDados()
+        {
+            Console.WriteLine("URL: " + _url);
+            Console.WriteLine("Versão: " + _versao);
+            Console.WriteLine("Porta: " + _porta);
+        }
+    }
+}
---
+++ PadroesDeProjeto/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace PadroesDeProjeto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Testando a classe configuração usando o padrão singleton");
+
+            ConfiguracaoSingleton config = ConfiguracaoSingleton.getInstance();
+            config.ExibirDados();
+
+
+            ConfiguracaoSingleton config2 = ConfiguracaoSingleton.getInstance();
+            config.ExibirDados();
+
+            if (config == config2)
+                Console.WriteLine("Mesma instancia");
+
+            // Linha de baixo, estou chamabndo o construtor no cliente
+            // Isso não podemos, pois criamos uma outra instancia. O construtor deve ser privado ou protegido!
+            //ConfiguracaoSingleton config3 = new();
+            ConfiguracaoSingleton config3 = ConfiguracaoSingleton.getInstance();
+            config.ExibirDados();
+
+            if (config == config3)
+                Console.WriteLine("Mesma instancia");
+            else
+                Console.WriteLine("Diferente instancia");
+
+        }
+    }
+}
